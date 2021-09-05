e8409d570f1a4f62a810be3940c6fed244f0c254
Rohan Bansal <bansalrohan18@gmail.com>
2021-09-05 16:02:06 -0700

    migrate from test repo

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,31 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import "bulma/css/bulma.css";
 
-const app = createApp(App)
-app.use(store).use(router).mount('#app')
-app.config.globalProperties.$api = 'http://localhost:3000'
+// Import the Auth0 configuration
+import { domain, clientId } from "../auth_config.json";
+
+// Import the plugin here
+import { Auth0Plugin } from "./auth";
+
+// Install the authentication plugin here
+Vue.use(Auth0Plugin, {
+  domain,
+  clientId,
+  onRedirectCallback: (appState) => {
+    router.push(
+      appState && appState.targetUrl
+        ? appState.targetUrl
+        : window.location.pathname
+    );
+  },
+});
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  render: (h) => h(App),
+}).$mount("#app");
